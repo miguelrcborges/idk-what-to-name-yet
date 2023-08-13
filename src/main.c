50037efc006fd60084cbb6c202203de3fccf9f90
Miguel Borges <up202004481@edu.fe.up.pt>
2023-08-13 23:58:10 +0100

    saving some more work
    I'll play league now

--- src/main.c
+++ src/main.c
@@ -5,6 +5,7 @@
 #include <SDL_video.h>
 
 #include "exitcodes.h"
+#include "scenes.h"
 
 #define STRFY(x) #x
 #define AT " in " __FILE__ ":" STRFY(__LINE__)
@@ -41,7 +42,7 @@ int main(int argc, char *argv[]) {
 		}
 	}
 
-	SDL_CreateWindow(
+	SDL_Window *win = SDL_CreateWindow(
 		"idk what to name yet",
 		SDL_WINDOWPOS_CENTERED,
 		SDL_WINDOWPOS_CENTERED,
@@ -49,8 +50,37 @@ int main(int argc, char *argv[]) {
 		height,
 		winflags
 	);
+	if (win == NULL) {
+		SDL_Log("Error " AT ": %s", SDL_GetError());
+		SDL_Quit();
+		return SDL_WINDOW_CREATION_ERROR;
+	}
 
+	SDL_Renderer *ren = SDL_CreateRenderer(win, -1, renflags);
+	if (ren == NULL) {
+		SDL_Log("Error " AT ": %s", SDL_GetError());
+		SDL_DestroyWindow(win);
+		return SDL_RENDERER_CREATION_ERROR;
+	}
+
+	enum SCENE current = MAIN_MENU;
+
+	while (1) {
+		switch (current) {
+		case QUIT:
+			goto close;
+		case MAIN_MENU:
+			break;
+		case IN_GAME: 
+			break;
+		default:
+			__builtin_unreachable();
+		}
+	}
+
+close:
+	SDL_DestroyRenderer(ren);
+	SDL_DestroyWindow(win);
 	SDL_Quit();
-
 	return 0;
 }
---
+++ src/scenes.h
@@ -0,0 +1,10 @@
+#ifndef SCENES_H
+#define SCENES_H
+
+enum SCENE {
+	QUIT,
+	MAIN_MENU,
+	IN_GAME,
+};
+
+#endif
